9af6e2d6a850cc52e15a7c6b4b73b881fd1fd3d9
luizhp <luizhp@yahoo.com>
2025-03-19 19:25:46 -0300

    feat: add main entry point with execution timer and logging

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"os"
+	"time"
+)
+
 func main() {
+
+	startTimerAll := time.Now()
+	log.Printf("🚀 Start\n")
+	defer func() {
+		log.Printf("🏆 Finished with sucess. It took %v\n", time.Since(startTimerAll))
+		os.Exit(0)
+	}()
 
 }
