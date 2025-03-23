8cf82552bb30dd7b1c3d02d3e3bdc91cad605667
luizhp <luizhp@yahoo.com>
2025-03-22 22:18:54 -0300

    feat: implement query files list handling

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"os"
 	"time"
+
+	"github.com/luizhp/query-extract/internal/infra/filesystem"
 )
 
 func main() {
@@ -14,6 +16,21 @@ func main() {
 		log.Printf("🏆 Finished with sucess. It took %v\n", time.Since(startTimerAll))
 		os.Exit(0)
 	}()
+
+	queriesFolder := "/home/user/Documents/dev/luizhp/QueryExtract/data/queries"
+	queriesExtension := "sql"
+	// outputFolder := "/home/user/Documents/dev/luizhp/QueryExtract/data/output"
+
+	// Get list of query files available
+	queries, err := filesystem.ListFolder(queriesFolder, queriesExtension)
+	if err != nil {
+		log.Printf("☠️ Error: %v\n", err)
+		os.Exit(1)
+	}
+	if len(queries) == 0 {
+		log.Printf("☠️ Error: No queries found at %v\n", queriesFolder)
+		os.Exit(1)
+	}
 
 	// Get Target DB connection
 
