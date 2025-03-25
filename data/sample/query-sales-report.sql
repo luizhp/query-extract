42fa860404714e28e5b0502f963d610314395d05
luizhp <luizhp@yahoo.com>
2025-03-25 20:40:53 -0300

    chore(queries): translate columns to english

--- data/sample/query-sales-report.sql
+++ data/sample/query-sales-report.sql
@@ -1,16 +1,16 @@
 SELECT 
-    orders.order_id AS id_pedido,
-    sellers.first_name AS nome_vendedor,
-    sellers.last_name AS sobrenome_vendedor,
-    products.product_name AS nome_produto,
-    suppliers.company_name AS nome_fornecedor,
-    clients.first_name AS nome_cliente,
-    clients.last_name AS sobrenome_cliente,
-    orders.order_date AS data_pedido,
-    order_lines.quantity AS quantidade,
-    order_lines.unit_price AS preco_unitario,
-    order_lines.discount AS desconto,
-    (order_lines.quantity * order_lines.unit_price * (1 - order_lines.discount)) AS total_linha
+    orders.order_id AS order_id,
+    sellers.first_name AS seller_first_name,
+    sellers.last_name AS seller_last_name,
+    products.product_name AS product_name,
+    suppliers.company_name AS supplier_name,
+    clients.first_name AS client_first_name,
+    clients.last_name AS client_last_name,
+    orders.order_date AS order_date,
+    order_lines.quantity AS quantity,
+    order_lines.unit_price AS unit_price,
+    order_lines.discount AS discount,
+    (order_lines.quantity * order_lines.unit_price * (1 - order_lines.discount)) AS line_total
 FROM 
     order_lines
 INNER JOIN 
