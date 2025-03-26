22182bb1373380f1c6ccbc8e7631959b1223cd43
luizhp <luizhp@yahoo.com>
2025-03-25 21:51:35 -0300

    refactor(job): organize to package folder

---
+++ src/pkg/job/interfaces.go
@@ -0,0 +1,16 @@
+package job
+
+import (
+	"database/sql"
+
+	"github.com/luizhp/query-extract/internal/entity"
+)
+
+type JobInterface interface {
+	GetDB() *sql.DB
+	GetFile() entity.File
+	GetResults() entity.Results
+	GetOutputFolder() string
+	Extract() error
+	Dump() error
+}
