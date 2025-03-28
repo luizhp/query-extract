bb55f22a8080b28ce11412223c765f06aced4029
luizhp <luizhp@yahoo.com>
2025-03-28 20:30:39 -0300

    chore(database): declares all columns on query

--- data/sample/sqlserver2k19/sqlserver-query.sql
+++ data/sample/sqlserver2k19/sqlserver-query.sql
@@ -1,1 +1,37 @@
-select * from DataTypesExample
+select 
+  TinyIntColumn
+, SmallIntColumn
+, IntColumn
+, BigIntColumn
+, DecimalColumn
+, NumericColumn
+, FloatColumn
+, RealColumn
+
+, DateColumn
+, TimeColumn
+, DateTimeColumn
+, SmallDateTimeColumn
+, DateTime2Column
+, DateTimeOffsetColumn
+
+, CharColumn
+, VarCharColumn
+, TextColumn
+, NCharColumn
+, NVarCharColumn
+, NTextColumn
+
+, BinaryColumn
+, VarBinaryColumn
+, ImageColumn
+, BitColumn
+, UniqueIdentifierColumn
+, XmlColumn
+, JsonColumn
+, GeographyColumn
+, GeometryColumn
+, SqlVariantColumn
+, HierarchyIdColumn
+
+from DataTypesExample
